1a0c1be2aead6f0f8e0b785af1e24b0d5cec9b51
“Alexander <alexsanderskywork@gmail.com>
2018-03-01 00:40:26 +0400

    completed

--- ImageRecognitionTest/ViewController.swift
+++ ImageRecognitionTest/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    
+    // MARK: - ML
+    
+    private let mobileNet = MobileNet()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - IBActions
+    
+    @IBAction private func presentPicker(_ button: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
-
+    
+    // MARK: - ML functions
+    
+    private func getImageInfo(_ image: UIImage) {
+        guard let pixelBuffer = ImageToPixelBufferConverter.convertToPixelBuffer(image: image) else { return }
+        do {
+            let result = try mobileNet.prediction(image: pixelBuffer)
+            resultLabel.text = result.classLabel
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+    }
 
 }
 
+// MARK: - UIImagePickerController Delegate
+
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let tempImage = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        imageView.image = tempImage
+        getImageInfo(tempImage)
+    }
+    
+}
